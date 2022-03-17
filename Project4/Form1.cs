781731458c66606cf4c8914e9e2ad352c4e771c5
vahid bakhtiary <vahidbakhtiary@hotmail.com>
2022-03-17 11:19:02 +0330

    session 05

---
+++ Project4/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project4
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Convert
+
+            //int varInt = 10;
+            // long varLong = 20;
+            //string varStr = varInt.ToString();
+            // string varStr = varLong.ToString();
+
+            string varStr = "10";
+            int varInt = int.Parse(varStr) ;
+            float varFloat = float.Parse(varStr);
+            long varLong = long.Parse(varStr);
+
+        }
+    }
+}
---
+++ Project4/Form2.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project4
+{
+    public partial class Form2 : Form
+    {
+
+        string firstNumber = "";
+        string secondNumber = "";
+        string opt = "";
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+         
+
+        private void number_click(object sender, EventArgs e)
+        {
+             
+            Button button = (Button) sender;
+            string number = button.Text;
+
+            if (string.IsNullOrWhiteSpace(opt))
+            {
+                firstNumber = firstNumber + number;
+            }else
+            {
+                secondNumber = secondNumber + number;
+            }
+           
+
+            string lastValue = txtResult.Text;
+
+            txtResult.Text = lastValue + number;
+
+        }
+
+        private void operator_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            opt = button.Text;
+
+            string lastValue = txtResult.Text;
+            txtResult.Text = lastValue + Environment.NewLine + opt + Environment.NewLine;
+        }
+
+        private void equal_click(object sender, EventArgs e)
+        {
+            string lastValue = txtResult.Text;
+
+            int result = calc( int.Parse(firstNumber) , int.Parse(secondNumber) ,  opt);
+
+            txtResult.Text = lastValue + Environment.NewLine + "=" +  Environment.NewLine+  result;
+        }
+
+        int calc(int num1 , int num2 , string opt)
+        {
+            switch(opt)
+            {
+
+                case "+":
+                  return num1 + num2;
+                case "-":
+                    return num1 - num2;
+                case "*":
+                    return num1 * num2;
+                default:
+                    return num1 / num2;
+            }
+        }
+
+        private void clear_click(object sender, EventArgs e)
+        {
+
+             firstNumber = "";
+             secondNumber = "";
+             opt = "";
+             txtResult.Text = "";
+        }
+
+        private void break_click(object sender, EventArgs e)
+        {
+            string lastValue = txtResult.Text;
+
+            if (lastValue.Length > 0)
+            {
+                string newStr = lastValue.Substring(0, lastValue.Length - 1);
+
+                txtResult.Text = newStr;
+            }
+               
+        }
+    }
+}
